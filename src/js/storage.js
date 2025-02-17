237439ecabe3a3f40cc79ff8b5e9e83bb892db1d
Ricardo Correa <ricardo1027diaz@gmail.com>
2025-02-17 12:01:00 -0500

    clase 2-17-25

---
+++ src/js/storage.js
@@ -0,0 +1,21 @@
+function saveLocal(){
+    const name = document.getElementById("nameLocalS").value;
+    localStorage.setItem("localName", name);
+
+    displayLocal()
+}
+
+function displayLocal(){
+    const localName = localStorage.getItem(localName);
+    document.getElementById("localOutput").textContent = localName ? 'Nombre guardado es: ${localname}' : 'No hay nombre guardado'
+}
+
+function saveSession(){
+    const name = document.getElementById("nameSessionS").value;
+    sessionStorage.setItem("sessionName", name);
+    displaySession();
+}
+function displaySession() {
+    const sessionName = sessionStorage.getItem("sessionName");
+    document.getElementById("sessionOutput").textContent = sessionName ? `Nombre guardado es ${sessionName}` : "No hay nombre guardado";
+}
